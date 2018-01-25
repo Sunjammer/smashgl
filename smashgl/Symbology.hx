ec63381556ed5750a96a6413f5c3c411a7792fcd
Andreas Rønning <andreas@doomsday.no>
2018-01-25 18:45:06 +0100

    Use inline strings for symbology shader source

--- smashgl/Symbology.hx
+++ smashgl/Symbology.hx
@@ -25,7 +25,29 @@ class Symbology
 
     static function initialize(){
         vertBuffer = GL.createBuffer();
-        shader = new Shader([Vertex("assets/shaders/symbology.vert"), Fragment("assets/shaders/symbology.frag")], "Symbology");
+
+
+        shader = new Shader(
+            [
+                Vertex("
+attribute vec2 aPosition;
+attribute vec2 aInfo;
+
+uniform vec2 uScreenSize;
+varying float vOpacity;
+
+void main(){
+    vec2 pos = ((aPosition/uScreenSize) * 2.0 - 1.0) * vec2(1, -1);
+    vOpacity = aInfo.y;
+    gl_PointSize = aInfo.x;
+    gl_Position = vec4(pos, 0.0, 1.0);
+}"), 
+                Fragment("
+varying float vOpacity;
+void main(){
+    gl_FragColor = vec4(1.1, 1.1, 1.1, vOpacity);
+}")
+            ], "Symbology");
 
         uScreenSize = shader.getUniform("uScreenSize");
         aPosition = shader.getAttribute("aPosition");
